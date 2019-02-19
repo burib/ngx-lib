64052fca83f38edbc0f28a54e0a55de9efc103d0
Balázs Buri <info@burib.net>
2019-02-19 15:04:29 +0100

    0.0.3

--- dist/ngx-lib/lib/ngx-lib.service.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class NgxLibService {
-    constructor();
-}
--- dist/ngx-lib/public_api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/ngx-lib.service';
-export * from './lib/ngx-lib.component';
-export * from './lib/ngx-lib.module';
-export * from './lib/components/footer/footer.component';
